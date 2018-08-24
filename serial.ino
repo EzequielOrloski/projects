db98ce11e8b6996f87b0a45034b1c89801d435fb
Ezequiel Orloski <42222212+EzequielOrloski@users.noreply.github.com>
2018-08-24 17:45:09 -0300

    Add files via upload

---
+++ serial.ino
@@ -0,0 +1,28 @@
+#define pin    13
+char resp;
+void setup() {
+  pinMode(pin, OUTPUT);
+  Serial.begin(9600);
+}
+void loop(){
+  if(Serial.available() > 0){
+    resp = Serial.read();
+    if(resp == '1'){
+        digitalWrite(pin, HIGH);
+        Serial.println("led aceso!");
+    }else{
+        digitalWrite(pin, LOW);
+        Serial.println("led apagado!");
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
